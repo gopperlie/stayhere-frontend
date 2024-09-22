da894817afdccb0b217757733f490ee1ce3b7742
MH S <gopperlie123@gmail.com>
2024-09-22 21:07:04 +0800

    feat date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import "./App.css";
+import * as React from "react";
 import { Greeting } from "./components/Greeting";
+import dayjs, { Dayjs } from "dayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Container } from "@mui/material";
 
 function App() {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs("2022-04-17"));
+
   return (
     <>
       <Greeting />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Container maxWidth="sm">
+          <DatePicker
+            label="Uncontrolled picker"
+            defaultValue={dayjs("2022-04-17")}
+          />
+          <DatePicker
+            label="Controlled picker"
+            value={value}
+            onChange={(newValue) => setValue(newValue)}
+          />
+        </Container>
+      </LocalizationProvider>
     </>
   );
 }
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,20 +1,3 @@
-// const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
-// import { useEffect, useState } from "react";
-
-// export function Greeting() {
-//   const [greeting, setGreeting] = useState(null);
-
-//   useEffect(() => {
-//     fetch(`${BACKEND_URL}/api/greeting`)
-//       .then((res) => res.json())
-//       .then((data) => setGreeting(data.greeting));
-//   }, [setGreeting]);
-
-//   if (!greeting) return null;
-
-//   return <h1 className="text-center mb-5">{greeting}</h1>;
-// }
-
 import { useEffect, useState } from "react";
 
 const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
@@ -44,3 +27,19 @@ export function Greeting() {
 
   return <h1 className="text-center mb-5">{greeting}</h1>;
 }
+// const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+// import { useEffect, useState } from "react";
+
+// export function Greeting() {
+//   const [greeting, setGreeting] = useState(null);
+
+//   useEffect(() => {
+//     fetch(`${BACKEND_URL}/api/greeting`)
+//       .then((res) => res.json())
+//       .then((data) => setGreeting(data.greeting));
+//   }, [setGreeting]);
+
+//   if (!greeting) return null;
+
+//   return <h1 className="text-center mb-5">{greeting}</h1>;
+// }
